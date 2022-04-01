cf2ccdaca8d6bd6946a72dc2aa2857f67973918d
Monkendrick <52892395+mcompass11@users.noreply.github.com>
2022-04-01 12:03:11 -0400

    implemented a scatter chart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import './App.css';
 import {EventList} from './EventList';
 import CitySearch from './CitySearch';
@@ -60,13 +61,36 @@ class App extends Component {
     });
     }
 
+    getData = () => {
+      const { locations, events } = this.state;
+      const data = locations.map((location) => {
+        const number = events.filter((event) => event.location === location).length
+        const city = location.split(', ').shift()
+        return {city, number};
+      })
+      return data;
+    };
+
   render() {
     return (
       <div className="App">
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
         <WarningAlert text={this.state.infoText} />
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} numberOfEvents={this.state.numberOfEvents} />
         <NumberOfEvents updateNumberOfEvents={(number) => {
           this.updateNumberOfEvents(number);}} updateEvents={this.updateEvents}/>
+
+        <h4>Events in each city</h4>
+        <ResponsiveContainer height={400}>
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis type="number" dataKey="number" name="# of events" allowDecimals={false} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#82ca9d" />
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList events={this.state.events} numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents}/>
       </div>
     );
