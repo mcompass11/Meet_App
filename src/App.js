48a8ea4dacee261bbb52f39b1ae5547a7fa42029
Monkendrick <52892395+mcompass11@users.noreply.github.com>
2022-04-09 14:51:59 -0400

    Revert "changed checkToken in api.js"
    This reverts commit aaa5acd4c2794aa1cf6d80d08ee1fd3642bfa9d9.

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ class App extends Component {
       });
     }
   }
-
   componentWillUnmount(){
     this.mounted = false;
   }
@@ -49,7 +48,6 @@ class App extends Component {
       this.updateEvents(this.state.currentLocation)
     );
   };
-  
   updateEvents = (location) => {
 
     getEvents().then((events) => {
@@ -61,7 +59,6 @@ class App extends Component {
       });
     });
     }
-
     getData = () => {
       const { locations, events } = this.state;
       const data = locations.map((location) => {
--- src/api.js
+++ src/api.js
@@ -2,6 +2,14 @@ import { mockData } from "./mock-data";
 import axios from 'axios';
 import NProgress from 'nprogress';
 
+/**
+ * 
+ * @param {*} events:
+ * The following function should be in the "api.js" file.
+ * This function takes an array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading aSet
+ * The Set will remove all duplicates from the array.
+ */
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
   var locations = [...new Set(extractLocations)];
@@ -11,7 +19,7 @@ export const extractLocations = (events) => {
 
 
 
-export const checkToken = async (accessToken) => {
+const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
     )
