f2163d2cbcd37796d14b69fed8bb6efde1761381
Monkendrick <52892395+mcompass11@users.noreply.github.com>
2022-04-09 14:52:38 -0400

    Revert "adjusted setupProxy"
    This reverts commit 2fb31fa129e721d79406004a9896193d7ef84d04.

--- src/App.js
+++ src/App.js
@@ -74,7 +74,6 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Meet App</h1>
-
         
         <h4>Choose your nearest city</h4>
         <WarningAlert text={this.state.infoText} />
